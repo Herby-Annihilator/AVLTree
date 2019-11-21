b8f0444046cdece5074e8ba84d11e9892777d7aa
Danil Rukin <danil2018d@yandex.ru>
2019-11-21 23:34:13 +0700

    Started writing menu for labs, class Subroutines

--- Program.cs
+++ Program.cs
@@ -18,6 +18,104 @@ namespace AlgLab7
             tree.Add(15, 15);
             tree.Add(15, 25);
             tree.Root = tree.LeftRotation(tree.Root);
+            bool goOut;
+            AVLTree<int> avlTree = new AVLTree<int>();
+            do
+            {
+                goOut = false;
+                switch (Subroutines.PrintMenu())
+                {
+                    //
+                    // c - создать дерево и заполнить его случайными величинами
+                    //
+                    case 'c':
+                        {
+                            int numberOfNodes;
+                            do
+                            {
+                                do
+                                {
+                                    Console.WriteLine("\nУкажите количество узлов в дереве (не более 100 и не менее 1)");
+                                } while (Int32.TryParse(Console.ReadLine(), out numberOfNodes));
+                            } while (numberOfNodes < 1 || numberOfNodes > 100);
+                            Random random = new Random();
+                            for (int i = 0; i < numberOfNodes; i++)
+                                avlTree.Add(random.Next(-50, 50), random.Next(-50, 50));
+                            random = null;
+                            Subroutines.SaveTreeInFile(avlTree, "input.dat");
+                            Console.WriteLine("\nДобавление прошло успешно, нажмите что-нибудь!");
+                            Console.ReadKey();
+                            break;
+                        }
+                    //
+                    // b - добавить элементы в дерево вручную
+                    //
+                    case 'b':
+                        {
+                            int numberOfNodes;
+                            int nodeKey;
+                            do
+                            {
+                                do
+                                {
+                                    Console.WriteLine("\nУкажите количество узлов в дереве, которое хотите добавить (не более 10 и не менее 1)");
+                                } while (Int32.TryParse(Console.ReadLine(), out numberOfNodes));
+                            } while (numberOfNodes < 1 || numberOfNodes > 10);
+                            for (int i = 0; i < numberOfNodes; i++)
+                            {
+                                do
+                                {
+                                    Console.WriteLine("Число №" + i + " = ");
+                                } while (Int32.TryParse(Console.ReadLine(), out nodeKey) == false);
+                                avlTree.Add(nodeKey, nodeKey);
+                            }
+                            Subroutines.SaveTreeInFile(avlTree, "input.dat");
+                            Console.WriteLine("\nДобавление прошло успешно, нажмите что-нибудь!");
+                            Console.ReadKey();
+                            break;
+                        }
+                    //
+                    // p - показать дерево (сделать обход, без связей)
+                    //
+                    case 'p':
+                        if (avlTree.ShowTree() == false)
+                        {
+                            Console.WriteLine("Дерево пусто! Нажмите что-нибудь");
+                            Console.ReadKey();
+                        }
+                        break;
+                    //
+                    // r - показать таблицу связей дерева
+                    //
+                    case 'r':
+                        if (avlTree.ShowTreeLinks() == false)
+                        {
+                            Console.WriteLine("Дерево пусто! Нажмите что-нибудь");
+                            Console.ReadKey();
+                        }
+                        break;
+                    //
+                    // h - Получить высоту дерева
+                    //
+                    case 'h':
+                        Console.WriteLine("Высота данного дерева = " + avlTree.Height);
+                        Console.ReadKey();
+                        break;
+                    //
+                    // v - получить информацию о корне
+                    //
+                    case 'v':
+                        avlTree.GetRootInfo();
+                        Console.ReadKey();
+                        break;
+                    //
+                    // ESC - выход
+                    //
+                    case (char)27:
+                        goOut = true;
+                        break;
+                }
+            } while (goOut == false);
         }
     }
 }
---
+++ Subroutines.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgLab7
+{
+    class Subroutines
+    {
+        /// <summary>
+        /// Печатает меню 
+        /// </summary>
+        /// <returns></returns>
+        public static char PrintMenu()
+        {
+            char symbol;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("* c - создать дерево и заполнить его случайными величинами");
+                Console.WriteLine("* b - добавить элементы в дерево вручную");
+                Console.WriteLine("* p - показать дерево (сделать обход, без связей)");
+                Console.WriteLine("* r - показать таблицу связей дерева");
+                Console.WriteLine("* h - Получить высоту дерева");
+                Console.WriteLine("* v - получить информацию о корне");
+                Console.WriteLine("* ESC - выход");
+                Console.Write("Ваш выбор - ");
+                symbol = Convert.ToChar(Console.ReadKey(true));
+            } while (symbol != 'c' && symbol != 'b' && symbol != 'p' && symbol != 'r' && symbol != 'h' && symbol != 'v' && symbol != 27);
+            return symbol;
+        }
+        //
+        // Сохранить в input.dat
+        //
+        public static void SaveTreeInFile(AVLTree<int> tree, string fileName)
+        {
+            StreamWriter writer = new StreamWriter(fileName);
+            
+            writer.Close();
+        }
+        //
+        // Переписать из файла в файл
+        //
+        /// <summary>
+        /// Переписывает содержимое одного файла в другой
+        /// </summary>
+        /// <param name="fileNameFrom">откуда переписывать</param>
+        /// <param name="fileNameTo">куда переписывать</param>
+        public static void WriteFromFileToFile(string fileNameFrom, string fileNameTo)
+        {
+            if (fileNameTo == fileNameFrom)
+            {
+                throw new Exception("Невозможно переписать из файла в тот же файл");
+            }
+            StreamReader reader = new StreamReader(fileNameFrom);
+            if (reader == null)
+            {
+                throw new Exception("Ошибка открытия файла для чтения");
+            }
+            StreamWriter writer = new StreamWriter(fileNameTo);
+            if (writer == null)
+            {
+                throw new Exception("Ошибка открытия файла для записи");
+            }
+            string toRewrite;
+            while ((toRewrite = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(toRewrite);
+            }
+            writer.Close();
+            reader.Close();
+        }
+        //
+        // Дополнить содержание одного файла содержаением другого
+        //
+        /// <summary>
+        /// Из первого файла дописывает данные во второй файл
+        /// </summary>
+        /// <param name="fileNameFrom">откуда переписывать</param>
+        /// <param name="fileNameTo">куда переписывать</param>
+        public static void AddFromFileToFile(string fileNameFrom, string fileNameTo)
+        {
+            if (fileNameTo == fileNameFrom)
+            {
+                throw new Exception("Невозможно переписать из файла в тот же файл");
+            }
+            StreamReader reader = new StreamReader(fileNameFrom);
+            StreamWriter writer = new StreamWriter(fileNameTo, true);
+            string toRewrite;
+            while ((toRewrite = reader.ReadLine()) != null)
+            {
+                writer.WriteLine(toRewrite);
+            }
+            writer.Close();
+            reader.Close();
+        }
+        //
+        // Получить целое число
+        //
+        public static int GetInt()
+        {
+            int number;
+            string strNum;
+            do
+            {
+                strNum = Console.ReadLine();
+            } while (Int32.TryParse(strNum, out number) == false);
+            return number;
+        }
+    }
+}
